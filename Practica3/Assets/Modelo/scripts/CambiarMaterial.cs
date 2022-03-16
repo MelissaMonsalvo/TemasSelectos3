ba9f931c00c64d8eac485d388b8940f92af4aac8
MelissaMonsalvo <43361335+MelissaMonsalvo@users.noreply.github.com>
2022-03-15 20:06:59 -0600

    Practica 3

---
+++ Practica3/Assets/Modelo/scripts/CambiarMaterial.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarMaterial : MonoBehaviour
+{
+     public GameObject esfera;
+     public Material Material360;
+     public Material Material3602;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int nummaterial=1;
+        Renderer rend = esfera.GetComponent<Renderer>();
+
+        foreach (Touch touch in Input.touches)
+        {
+            
+            if(nummaterial ==1)
+            {
+                nummaterial=2;
+            }else{
+                nummaterial=1;
+            }
+        
+            switch(nummaterial)
+            {
+                case 1:
+                    rend.material = Material360;
+                    break;
+                case 2:
+                    rend.material = Material3602;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
